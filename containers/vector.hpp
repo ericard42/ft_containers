04caaf7c9522876ae843e874e3447a78efa570ab
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2022-03-25 18:32:08 +0100

    Vecot

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -3,6 +3,7 @@
 
 #include "../iterators/reverse_iterator.hpp"
 #include "../iterators/iterator_traits.hpp"
+#include "../iterators/random_iterator.hpp"
 #include <memory>
 
 namespace ft {
@@ -16,11 +17,54 @@ namespace ft {
 			typedef typename	allocator_type::const_reference const_reference;
 			typedef typename	allocator_type::pointer pointer;
 			typedef typename	allocator_type::const_pointer const_pointer;
-			/*-42*/typedef typename	std::vector<value_type>::iterator iterator; //A modifier
-			/*-42*/typedef typename	std::vector<const value_type>::iterator const_iterator; //A modifier
-			typedef typename	reverse_iterator<iterator> reverse_iterator;
+			typedef typename	ft::random_iterator<value_type> iterator;
+			typedef typename	ft::random_iterator<const value_type> const_iterator;
+			typedef typename	ft::reverse_iterator<iterator> reverse_iterator;
+			typedef typename	ft::reverse_iterator<const_iterator> const_reverse_iterator;
+			typedef typename 	ft::iterator_traits<iterator>::difference_type difference_type;
+			typedef typename	allocator_type::size_type 	size_type;
+
+			//Default Constructor
+			explicit vector(const allocator_type &alloc = allocator_type()) : _alloc(alloc),
+																				_first(0),
+																				_last(0),
+																				_max (0) {
+
+			}
+			//fill constructor
+			explicit vector(size_type n, const value_type &val = value_type(),
+							const allocator_type &alloc = allocator_type()) : _alloc(alloc),
+																				_first(0),
+																				_last(0),
+																				_max(0) {
+				_first = _alloc.allocate(n);
+				_max = _first + n;
+				_last = _first;
+				while (_last < _max)
+				{
+					_alloc.construct(_last, val);
+					_last++;
+				}
+			}
+			//range constructor
+			template <class InputIterator>
+			vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type()) {
+
+			}
+			//copy constructor
+			vector(const vector &x) {
+
+			}
+			//destructor
+			~vector() {
+
+			}
 
 		private :
+			allocator_type _alloc;
+			pointer _first;
+			pointer _last;
+			pointer _max;
 	};
 }
 
