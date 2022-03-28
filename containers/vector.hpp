1bdcdd34295c6dd96dcdaebbcb85a0af7b7810b0
Emily FLYNN Elodie RICARD <ericard@student.42.fr>
2022-03-28 15:55:44 +0200

    Vector++

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -4,7 +4,9 @@
 #include "../iterators/reverse_iterator.hpp"
 #include "../iterators/iterator_traits.hpp"
 #include "../iterators/random_iterator.hpp"
+#include "../utils/utils.hpp"
 #include <memory>
+#include
 
 namespace ft {
 	template < class T, class Allocator = std::allocator<T> >
@@ -40,7 +42,7 @@ namespace ft {
 				_first = _alloc.allocate(n);
 				_max = _first + n;
 				_last = _first;
-				while (_last < _max)
+				while (_last <= _max)
 				{
 					_alloc.construct(_last, val);
 					_last++;
@@ -48,15 +50,186 @@ namespace ft {
 			}
 			//range constructor
 			template <class InputIterator>
-			vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type()) {
-
+			vector(InputIterator first, InputIterator last,
+				   const allocator_type &alloc = allocator_type()) : _alloc(alloc),
+				   														_first(0),
+																		_last(0),
+																		_max(0) {
+				difference_type n = ft::distance(first, last);
+				_first = _alloc.allocate(n);
+				_max = _first + n;
+				_last = _first;
+				while (_last <= _max)
+				{
+					_alloc.construct(_last, *first++);
+					_last++;
+				}
 			}
 			//copy constructor
-			vector(const vector &x) {
-
+			vector(const vector &x) : _alloc(x.alloc),
+										_first(0),
+										_last(0),
+										_max(0) {
+				insert(begin(), x.begin(), x.end());
 			}
 			//destructor
 			~vector() {
+				clear();
+				_alloc.deallocate(_start, capacity());
+			}
+
+			//Operator =
+			vector &operator=(const vector &x) {
+				if (x == *this)
+					return (*this);
+				clear();
+				insert(begin(), x.begin(), x.end());
+				return (*this);
+			}
+
+			//Iterators
+			iterator begin() {
+				return (_first);
+			}
+			const_iterator begin() const {
+				return (_first);
+			}
+
+			iterator end() {
+				if (empty() == true)
+					return (begin());
+				return (_last);
+			}
+			const_iterator end() const {
+				if (empty() == true)
+					return (begin());
+				return (_last);
+			}
+
+			reverse_iterator rbegin() {
+				return (reverse_iterator(end()));
+			}
+			const_reverse_iterator rbegin() const {
+				return (reverse_iterator(end()));
+			}
+
+			reverse_iterator rend() {
+				return (reverse_iterator(begin()));
+			}
+			const_reverse_iterator rend() const {
+				return (reverse_iterator(begin()));
+			}
+
+
+			//Capacity
+			size_type size() const {
+				return (_last - _first);
+			}
+
+			size_type max_size() const {
+				return (allocator_type().max_size());
+			}
+
+			void resize(size_type n, value_type val = value_type()) {
+				if (n > max_size())
+					throw (std::length_error("vector::out of range"));
+				if (n > size())
+					insert(end(), n - size(), val);
+				while (n < size())
+				{
+					_last--;
+					_alloc.destroy(_last);
+				}
+			}
+
+			size_type capacity() const {
+
+			}
+
+			bool empty() const {
+
+			}
+
+			void reserve(size_type n) {
+
+			}
+
+
+			//Element access
+			reference operator[](size_type n) {
+
+			}
+			const_reference operator[](size_type n) const {
+
+			}
+
+			reference at(size_type n) {
+
+			}
+			const_reference at(size_type n) const {
+
+			}
+
+			reference front() {
+
+			}
+			const_reference  front() const {
+
+			}
+
+			reference back() {
+
+			}
+			const_reference back() const {
+
+			}
+
+
+			//Modifiers
+			template <class InputIterator>
+			void assign(InputIterator first, InputIterator last) {
+
+			}
+			void assign(size_type n, const value_type &val) {
+
+			}
+
+			void push_back(const value_type &val) {
+
+			}
+
+			void pop_back() {
+
+			}
+
+			iterator insert(iterator position, const value_type &val) {
+
+			}
+			void insert(iterator  position, size_type n, const value_type &val) {
+
+			}
+			template <class InputIterator>
+			void insert(iterator position, InputIterator first, InputIterator last) {
+
+			}
+
+			iterator erase(iterator position) {
+
+			}
+			iterator erase(iterator first, iterator last) {
+
+			}
+
+			void swap(vector &x) {
+
+			}
+
+			void clear() {
+
+			}
+
+			//Allocator
+			allocator_type get_allocator() const {
 
 			}
 
@@ -66,6 +239,38 @@ namespace ft {
 			pointer _last;
 			pointer _max;
 	};
+
+	//relational operators
+	template <class T, class Alloc>
+	bool operator==(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+
+	}
+	template <class T, class Alloc>
+	bool operator!=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+
+	}
+	template <class T, class Alloc>
+	bool operator<(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+
+	}
+	template <class T, class Alloc>
+	bool operator<=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+
+	}
+	template <class T, class Alloc>
+	bool operator>(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+
+	}
+	template <class T, class Alloc>
+	bool operator>=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+
+	}
+
+	//Swap
+	template <class T, class Alloc>
+	void swap(vector<T, Alloc> &x. vector<T, Alloc> &y) {
+
+	}
 }
 
 #endif
