2cde6a44880310eb9d48dd182577a142523dd407
ericard42 <ericard@student.42.fr>
2022-04-26 16:09:29 +0200

    Starting Stack

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -1,4 +1,46 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+#include "vector.hpp"
+
+namespace ft {
+	template <class T, class Container = vector<T> >
+	class stack {
+		public :
+			//Member Type
+			typedef T								value_type;
+			typedef Container						container_type;
+			typedef container_type::reference		reference;
+			typedef container_type::const_reference	const_reference;
+			typedef size_t							size_type;
+
+			//Constructor
+			//Destructor
+			//Operator=
+
+			//Empty
+
+			//Size
+
+			//Top
+
+			//Push
+
+			//Pop
+
+		protected :
+			container_type c;
+
+	};
+}
+
+//Relational Operators
+//Operator==
+//Operator!=
+//Operator<
+//Operator<=
+//Operator>
+//Operator>=
+
+
 #endif
