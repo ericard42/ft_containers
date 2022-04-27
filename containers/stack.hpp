5005bfeef98da00d7a30d7e31fff3b37f05a81bb
ericard42 <ericard@student.42.fr>
2022-04-27 11:14:36 +0200

    stack, yay

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -10,37 +10,92 @@ namespace ft {
 			//Member Type
 			typedef T								value_type;
 			typedef Container						container_type;
-			typedef container_type::reference		reference;
-			typedef container_type::const_reference	const_reference;
 			typedef size_t							size_type;
 
 			//Constructor
+			explicit stack(const container_type &cont = container_type()) : c(cont) {
+
+			}
 			//Destructor
+			~stack() {
+
+			}
 			//Operator=
+			stack &operator=(const stack &other) {
+				return (c = other.c);
+			}
 
 			//Empty
+			bool empty() {
+				return (c.empty());
+			}
 
 			//Size
+			size_type size() {
+				return (c.size());
+			}
 
 			//Top
+			value_type &top() {
+				return (c.back());
+			}
+			const value_type &top() const {
+				return (c.back());
+			}
 
 			//Push
+			void push(value_type value) {
+				c.push_back(value);
+			}
 
 			//Pop
+			void pop() {
+				c.pop_back();
+			}
+
+			template <class Tf, class Containerf>
+			friend bool operator==(const stack<Tf, Containerf> &lhs, const stack<Tf, Containerf> &rhs);
+			template <class Tf, class Containerf>
+			friend bool operator<(const stack<Tf, Containerf> &lhs, const stack<Tf, Containerf> &rhs);
 
 		protected :
 			container_type c;
 
 	};
-}
 
-//Relational Operators
-//Operator==
-//Operator!=
-//Operator<
-//Operator<=
-//Operator>
-//Operator>=
+	//Relational Operators
+	//Operator==
+	template <class T, class Container>
+	bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return (lhs.c == rhs.c);
+	}
+	//Operator!=
+	template <class T, class Container>
+	bool operator!=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return (!(lhs == rhs));
+	}
+	//Operator<
+	template <class T, class Container>
+	bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return (lhs.c < rhs.c);
+	}
+	//Operator<=
+	template <class T, class Container>
+	bool operator<=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return (!(rhs < lhs));
+	}
+	//Operator>
+	template <class T, class Container>
+	bool operator>(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return (rhs < lhs);
+	}
+	//Operator>=
+	template <class T, class Container>
+	bool operator>=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+		return (!(lhs < rhs));
+	}
+
+}
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,31 +1,15 @@
 #include <iostream>
 #include "containers/vector.hpp"
+#include "containers/stack.hpp"
 #include <vector>
 
 int main() {
 
-	{
-		ft::vector<int> prout(0);
-		//for (int i = 0; i < 6; i++)
-		//	prout.push_back(i + 1);
-		ft::vector<int>::iterator poulet;
-		poulet = prout.begin();
-		prout.insert(poulet, 3,42);
-		for (size_t i = 0; i < prout.size(); i++)
-			std::cout << prout.at(i) << std::endl;
-		std::cout << prout.capacity() << std::endl;
-	}
-	{
-		std::cout << std::endl;
-		std::vector<int> prout(0);
-		//for (int i = 0; i < 6; i++)
-		//	prout.push_back(i+1);
-		std::vector<int>::iterator poulet;
-		poulet = prout.begin();
-		prout.insert(poulet, 3, 42);
-		for (size_t i = 0; i < prout.size(); i++)
-			std::cout << prout.at(i) << std::endl;
-		std::cout << prout.capacity() << std::endl;
+	ft::stack<int> prout;
 
-	}
+	prout.push(4);
+	prout.push(42);
+	std::cout << prout.top() << std::endl;
+	prout.pop();
+	std::cout << prout.top() << std::endl;
 }
